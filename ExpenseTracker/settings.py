a82534962f16e49caa54dc6b4e41518b992a1237
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-11-26 13:57:26 +0530

    Updated static in settings

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'Expense'
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
