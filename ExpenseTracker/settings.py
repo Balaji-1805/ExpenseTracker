9c688073e684eb91f3f559daf215b4ced2833296
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-11-26 13:56:07 +0530

    Debug changes to False

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#qh)wqzu0lq9_@f!5+x97%y8rqbvof8lh1j2i!_(kcmx$1d7i*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
